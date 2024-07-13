662164c6ac9f04469114dbfffec6b59cd1289a73
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2024-07-13 15:55:24 +0330

    register view and login view and logout view And their templates were added

--- BLOGs/urls.py
+++ BLOGs/urls.py
@@ -22,5 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
+    path('',include('accounts.urls'))
+
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import *
-
+app_name = 'blog'
 urlpatterns = [
     path('', index, name='index')
 
