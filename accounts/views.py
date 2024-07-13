80584eb4e1bb08b2263f1c249a057eb1e521ebb6
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2024-07-13 15:56:27 +0330

    register view and login view and logout view And their templates were added

---
+++ accounts/views.py
@@ -0,0 +1,39 @@
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+
+
+# Create your views here.
+def user_login(request):
+    if request.user.is_authenticated:
+        return redirect('blog:index')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('blog:index')
+
+    return render(request, template_name='forms/login_form.html')
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('blog:index')
+
+
+def user_register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+        if password == password2:
+            user = User(username=username, email=email, password=password)
+            user.set_password(password)
+            user.save()
+            return redirect('blog:index')
+        else:
+            return
+    return render(request, template_name='forms/registeration.html')
